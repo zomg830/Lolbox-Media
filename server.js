eac37a0327706728be45f1f82af95aea32284eea
zomg830 <matt.faubel@gmail.com>
2019-06-04 12:38:25 -0500

    Added compression (express middleware)

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const compression = require("compression");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
