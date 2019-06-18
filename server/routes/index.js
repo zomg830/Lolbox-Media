c13b809dc18003c1f7fe81b04fa096fc0aafc048
zomg830 <matt.faubel@gmail.com>
2019-06-18 09:49:48 -0500

    Retrying heroku deployment

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,15 @@
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./api");
-const authRoutes = require("./auth");
-const commentRoutes = require("./comment");
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const authRoutes = require('./auth');
+const commentRoutes = require('./comment');
 
 // API Routes
-router.use("/api", apiRoutes);
-router.use("/auth", authRoutes);
-router.use("/comment", commentRoutes);
+router.use('/api', apiRoutes);
+router.use('/auth', authRoutes);
+router.use('/comment', commentRoutes);
 
 // If no API routes are hit, send the React app
-router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
-);
+router.use((req, res) => res.sendFile(path.join(__dirname, 'client/build')));
 
 module.exports = router;
