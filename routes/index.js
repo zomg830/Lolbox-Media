f7ebaf39c68c027b7168b188fa12261879746ccc
zomg830 <matt.faubel@gmail.com>
2019-06-18 10:21:00 -0500

    retrying heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.use('/auth', authRoutes);
 router.use('/comment', commentRoutes);
 
 // If no API routes are hit, send the React app
-router.use((req, res) => res.sendFile(path.join('app', 'client/build')));
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
 
 module.exports = router;
